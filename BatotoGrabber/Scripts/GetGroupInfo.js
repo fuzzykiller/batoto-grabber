95557b072a60faf68a93ad8ded62b0838326c880
Daniel B <daniel.betz@daniel-betz.com>
2018-01-19 20:43:22 +0100

    Improve JavaScript language use; make GetSeriesInfo more robust

--- BatotoGrabber/Scripts/GetGroupInfo.js
+++ BatotoGrabber/Scripts/GetGroupInfo.js
@@ -1,7 +1,6 @@
 ﻿(function () {
     function extractKeyValue(/** @type {Element} */ row) {
-        let keyCell = row.querySelector("td:first-of-type");
-        let valueCell = row.querySelector("td:last-of-type");
+        let [keyCell, valueCell] = row.querySelectorAll("td");
 
         let key = keyCell.textContent.substring(0, keyCell.textContent.length - 1);
         let value = valueCell.textContent;
@@ -17,7 +16,7 @@
     }
 
     let metaDataTableEntries = [...document.querySelectorAll(".ipsBox_withphoto + br + .ipsBox table tr")]
-        .filter(x => x.querySelectorAll("td").length == 2);
+        .filter(x => x.querySelectorAll("td").length === 2);
     let metaData = metaDataTableEntries.map(extractKeyValue);
 
     return JSON.stringify(metaData);
--- BatotoGrabber/Scripts/GetSeriesInfo.js
+++ BatotoGrabber/Scripts/GetSeriesInfo.js
@@ -1,7 +1,6 @@
 ﻿(function () {
 	function extractKeyValue(/** @type {Element} */ row) {
-		let keyCell = row.querySelector("td:first-of-type");
-		let valueCell = row.querySelector("td:last-of-type");
+		let [keyCell, valueCell] = row.querySelectorAll("td");
 
 		let key = keyCell.textContent.substring(0, keyCell.textContent.length - 1);
 		let value = valueCell.textContent;
@@ -29,16 +28,12 @@
 	}
 
 	let metaDataTableEntries = [...document.querySelectorAll(".ipsBox_withphoto + br + .ipsBox table tr")]
-		.filter(x => x.querySelectorAll("td").length == 2);
+		.filter(x => x.querySelectorAll("td").length === 2);
 	let metaData = metaDataTableEntries.map(extractKeyValue);
 
 	function extractChapterInfo(/** @type {Element} */ row) {
 		let cells = row.querySelectorAll("td");
-		let titleCell = cells[0];
-		let languageCell = cells[1];
-		let groupsCell = cells[2];
-		let contributorCell = cells[3];
-		let dateCell = cells[4];
+		let [titleCell, languageCell, groupsCell, contributorCell, dateCell] = cells;
 
 		let groupInfos = [...groupsCell.querySelectorAll("a")].map(x => ({ name: x.textContent, url: x.href}));
 
@@ -55,12 +50,12 @@
 	let chapterTableEntries = [...document.querySelectorAll(".chapters_list tr.chapter_row")];
 	let chapterInfos = chapterTableEntries.map(extractChapterInfo);
 
-	let imageElement = /** @type {HTMLImageElement} */ (document.querySelector(".ipsBox_withphoto ~ .ipsBox > div > div > img"));
+	let imageElement = /** @type {HTMLImageElement} */ (document.querySelector(".ipsBox_withphoto + br + .ipsBox > div > div > img"));
 
 	let seriesInfo = {
 		metaData: metaData,
 		chapters: chapterInfos,
-		image: imageElement.src
+		image: imageElement && imageElement.src
 	};
 
 	return JSON.stringify(seriesInfo);
